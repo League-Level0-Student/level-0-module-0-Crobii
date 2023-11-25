5ca63176200532ad8b106f5ff7e7da7dd58e277d
league <54760319+0C3AN15@users.noreply.github.com>
2023-11-25 14:25:32 -0800

    I created world domination and a pop quiz!

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -4,15 +4,21 @@ package _03_print_and_popups._3_world_domination;
 import javax.swing.JOptionPane;
 
 public class WorldDomination {
-	
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) { 
+
 		// 1. Ask the user if they know how to write code.
+		String answer = JOptionPane.showInputDialog("Do you know how to write code?");
+		if(answer.contentEquals("yes")) 
+		{JOptionPane.showMessageDialog(null, "Then you're gonna rule the world!");
 
-		// 2. If they say "yes", tell them they will rule the world.
+		}
+		if(answer.contentEquals("no")) 
+		{JOptionPane.showMessageDialog(null, "Then sign up for classes at The League of Amazing Programmers, idiot!");
 
+		// 2. If they say "yes", tell them they will rule the world
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
+		}
 	}
 }
-
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,37 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
-		
+
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
-				// 2.  Ask the user a question 
-				
-				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
-		
+		JOptionPane.showMessageDialog(null, "QUIZ TIME!!!!");
+		// 2.  Ask the user a question 
+		String answer =JOptionPane.showInputDialog("What Pokemon is weak to fire, water, and grass?");
+		if(answer.contentEquals("Mamoswine")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		score = score + 1;	
+		}
+		// 3.  Use an if statement to check if their answer is correct
+		else {
+			JOptionPane.showMessageDialog(null, "wrong...");}
+		// 4.  if the user's answer was correct, add one to their score 
+		answer =JOptionPane.showInputDialog("What is the only Pokemon line that is poison AND flying type?");
+		if(answer.contentEquals("Zubat")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		score = score + 1;	
+		}
+		// 3.  Use an if statement to check if their answer is correct
+		else {
+			JOptionPane.showMessageDialog(null, "wrong...");}
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
-				// Option: Subtract a point from their score for a wrong answer
-		
-		
+		// Option: Subtract a point from their score for a wrong answer
+		JOptionPane.showMessageDialog (null, "Wowee! Your score was " +score );
+
 		// After all the questions have been asked, tell the user their final score 
-		
+
 	}
 }
